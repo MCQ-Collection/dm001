0ae16c97485ff50d010ba84f86bea506d7acaaac
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-10-08 02:59:52 +0300

    Add files via upload

---
+++ dmcns.js
@@ -0,0 +1,27 @@
+﻿//<![CDATA[
+$(document).ready(function() {
+    $('iframe[src*="cdn.rawgit.com"]').wrap('<div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;margin:0 auto;width:100%"></div>');
+    $('iframe[src*="cdn.rawgit.com"]').css({
+        "position": "absolute",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "border": "0"
+    });
+    $('iframe[src*="cdn.rawgit.com"]').each(function() {
+        $(this).attr("data-src", $(this).attr("src"));
+        $(this).attr("src", "")
+    });
+});
+
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i = 0; i < vidDefer.length; i++) {
+        if (vidDefer[i].getAttribute('data-src')) {
+            vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+        }
+    }
+}
+window.onload = init;
+//]]>
